e1ddf4140f65223a5d30538cb29c39b774afce7a
mohamed <mohamedsavula35@gmail.com>
2022-09-01 00:32:29 +0200

    /

--- analytice_sales_line/models/models.py
+++ analytice_sales_line/models/models.py
@@ -6,4 +6,9 @@ from odoo import models, fields
 class SaleOrderLineInherit(models.Model):
     _inherit = 'sale.order.line'
 
-    analytic_account_id = fields.Many2one(related="order_id.analytic_account_id", readonly=False)
+    analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account')
+
+    def _prepare_invoice_line(self, **optional_values):
+        invoice_line = super(SaleOrderLineInherit, self)._prepare_invoice_line(**optional_values)
+        invoice_line['analytic_account_id'] = self.analytic_account_id.id
+        return invoice_line
